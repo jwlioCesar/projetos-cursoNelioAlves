98709bb2c85c911f81721e5fb247786b30a47d23
Júlio César <juliocfds20@gmail.com>
2023-03-22 14:18:46 -0300

    commit teste

--- projeto_josi5/src/exceptions/DimensaoInvalidaException.java
+++ projeto_josi5/src/exceptions/DimensaoInvalidaException.java
@@ -1,6 +1,8 @@
 package exceptions;
 
 public class DimensaoInvalidaException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
 
 	public DimensaoInvalidaException(String msg) {
 		super(msg);
